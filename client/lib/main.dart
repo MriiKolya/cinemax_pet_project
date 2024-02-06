14462a6bafca6cf970206976cc5df58abcd0b1a2
MriiKolya <kolyamriy16@gmail.com>
2024-02-06 21:20:40 +0200

    add icons and color

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ui_kit/assets/icons/cinemax_icons.dart';
 
 void main() {
   runApp(const Cinemax());
@@ -15,6 +16,57 @@ class Cinemax extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
+      home: const MyIcons(),
     );
+  }
+}
+
+class MyIcons extends StatelessWidget {
+  const MyIcons({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    List<IconData> list = [
+      CinemaxIcons.alert,
+      CinemaxIcons.arrowBack,
+      CinemaxIcons.arrowIosDownward,
+      CinemaxIcons.audio,
+      CinemaxIcons.calendar,
+      CinemaxIcons.clock,
+      CinemaxIcons.closedCaption,
+      CinemaxIcons.devices,
+      CinemaxIcons.download,
+      CinemaxIcons.downloadForOfflineBlack24dp,
+      CinemaxIcons.edit,
+      CinemaxIcons.eyeOff,
+      CinemaxIcons.film,
+      CinemaxIcons.finish,
+      CinemaxIcons.fullscreen,
+      CinemaxIcons.globe,
+      CinemaxIcons.hd,
+      CinemaxIcons.heart,
+      CinemaxIcons.home,
+      CinemaxIcons.layer_2,
+      CinemaxIcons.notification,
+      CinemaxIcons.padlock_1,
+      CinemaxIcons.pause,
+      CinemaxIcons.person,
+      CinemaxIcons.profile,
+      CinemaxIcons.question,
+      CinemaxIcons.remove,
+      CinemaxIcons.search,
+      CinemaxIcons.settings,
+      CinemaxIcons.share,
+      CinemaxIcons.shield,
+      CinemaxIcons.star,
+      CinemaxIcons.trashBin,
+      CinemaxIcons.workspacePremiumBlack24dp
+    ];
+    return Scaffold(
+        body: SafeArea(
+      child: ListView.builder(
+          itemCount: list.length,
+          itemBuilder: (context, index) => Icon(list[index])),
+    ));
   }
 }
---
+++ ui_kit/lib/theme/color_scheme.dart
@@ -0,0 +1,22 @@
+import 'dart:ui';
+
+abstract class PrimaryColor {
+  static Color dark = const Color(0xff1F1D2B);
+  static Color soft = const Color(0xff252836);
+  static Color blueAccent = const Color(0xff12CDD9);
+}
+
+abstract class SecondaryColor {
+  static Color green = const Color(0xff22B07D);
+  static Color orane = const Color(0xffFF8700);
+  static Color red = const Color(0xffFF7256);
+}
+
+abstract class TextColor {
+  static Color black = const Color(0xff171725);
+  static Color darkGrey = const Color(0xff696974);
+  static Color whiteGrey = const Color(0xffF1F1F5);
+  static Color grey = const Color(0xff92929D);
+  static Color white = const Color(0xffFFFFFF);
+  static Color lineDark = const Color(0xffEAEAEA);
+}
