bf0b8dd0ad7b86ffd9144d4694b45c700e077e49
MriiKolya <kolyamriy16@gmail.com>
2024-02-13 15:15:42 +0200

    firebase flavor android

--- client/lib/main_dev.dart
+++ client/lib/main_dev.dart
@@ -9,7 +9,14 @@ import 'package:ui_kit/theme/dark_theme.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+      apiKey: 'AIzaSyCWnIln1jE6qtLoB--lTEiad4yGFywqQsk',
+      appId: '1:194431657319:android:44a531d346d487865bfbc2',
+      messagingSenderId: '1:194431657319',
+      projectId: 'cinemax-dev-a8193',
+    ),
+  );
   final devConfig = FlavorConfig(
     flavor: Flavor.dev,
     values: FlavorValues(),
--- client/lib/main_prod.dart
+++ client/lib/main_prod.dart
@@ -9,7 +9,15 @@ import 'package:ui_kit/theme/dark_theme.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+      apiKey: 'AIzaSyAhTRxvO1pf7WVbC0E2ZEyCyaWObOpAX68',
+      appId: '1:182735058765:android:743369296169a5618ee904',
+      messagingSenderId: '1:182735058765',
+      projectId: 'cinemax--prod',
+    ),
+  );
   final devConfig = FlavorConfig(
     flavor: Flavor.prod,
     values: FlavorValues(),
